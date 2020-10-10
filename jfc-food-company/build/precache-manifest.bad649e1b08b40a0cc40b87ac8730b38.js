6f6f7e876396f9a6405af0d535ac578dc9a49196
Christina Harris <christinathedesigner@outlook.com>
2020-10-10 08:48:17 -0700

    looking into the double node_module issue

---
+++ jfc-food-company/build/precache-manifest.bad649e1b08b40a0cc40b87ac8730b38.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "afb99fbb8cb7251e7d78fd0c821bfbd3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b582d32a219b96ccc428",
+    "url": "/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "2a95bcb09484c1f9220b",
+    "url": "/static/css/main.809a9822.chunk.css"
+  },
+  {
+    "revision": "b582d32a219b96ccc428",
+    "url": "/static/js/2.935164a7.chunk.js"
+  },
+  {
+    "revision": "d2966845b94a3318bf32eecc7af8015d",
+    "url": "/static/js/2.935164a7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2a95bcb09484c1f9220b",
+    "url": "/static/js/main.995b7793.chunk.js"
+  },
+  {
+    "revision": "04e84a84edbfc18bdfb3",
+    "url": "/static/js/runtime-main.6aad881c.js"
+  },
+  {
+    "revision": "2607f2426088abde5508403f0fcc84c5",
+    "url": "/static/media/Logo-Placeholder.2607f242.png"
+  }
+]);
